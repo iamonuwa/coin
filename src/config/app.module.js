c1e93d00a69f24914740fa36128bdd1993db3c85
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2017-03-09 22:29:21 +0100

    Added Application Configuration Controller

--- src/config/app.module.js
+++ src/config/app.module.js
@@ -12,13 +12,17 @@ angular.module('coin', [
     'ngMap',
     'ngMessages'
 	])
-.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 'cfpLoadingBarProvider', function ($stateProvider, $urlRouterProvider, $locationProvider, cfpLoadingBarProvider) {
-	$urlRouterProvider.otherwise('/');
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 'cfpLoadingBarProvider', '$rootScopeProvider', function ($stateProvider, $urlRouterProvider, $locationProvider, cfpLoadingBarProvider, $rootScopeProvider) {
+	// Angular sets a limit of 10 iterations per digest cycle. 
+  // But am setting it to 1000 iterations per digest cycle. This will assist in loading firebase data over a thin network.
+  // $rootScopeProvider.digestTtl(1000);
+
+  $urlRouterProvider.otherwise('/');
 	// $locationProvider.hashPrefix('');
 
   // cfpLoadingBarProvider.includeSpinner = true;
   cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
-  cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner">Custom Loading Message...</div>';
+  // cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner">Custom Loading Message...</div>';
 
 	$stateProvider
 		.state('public', {
@@ -101,7 +105,7 @@ angular.module('coin', [
       }
     })
     .state('app.buy', {
-      url: '/buy',
+      url: '/apply-for-exchange',
       templateUrl: 'views/buy.views.client.html',
       pageTitle: 'Coin Exchange Application Form',
       public: false,
@@ -114,8 +118,41 @@ angular.module('coin', [
         }]
       }
     })
+    .state('app.settings', {
+      url: '/admin/application-settings',
+      templateUrl: 'views/admin/settings.views.client.html',
+      pageTitle: 'Coin Exchange Application Form',
+      public: false,
+      resolve: {
+        // controller will not be loaded until $waitForSignIn resolves
+        // Auth refers to our $firebaseAuth wrapper in the factory below
+        "currentAuth": ["Auth", function(Auth) {
+          // $waitForSignIn returns a promise so the resolve waits for it to complete
+          return Auth.$requireSignIn();
+        }]
+      }
+    })
+    .state('app.users', {
+      url: '/admin/users',
+      templateUrl: 'views/admin/settings.views.client.html',
+      pageTitle: 'Coin Exchange Application Form',
+      public: false,
+      resolve: {
+        // controller will not be loaded until $waitForSignIn resolves
+        // Auth refers to our $firebaseAuth wrapper in the factory below
+        "currentAuth": ["Auth", function(Auth) {
+          // $waitForSignIn returns a promise so the resolve waits for it to complete
+          return Auth.$requireSignIn();
+        }]
+      }
+    })
+    .state('admin', {
+      abstract: true,
+      templateUrl: 'views/templates/admin.templates.views.client.html',
+      pageTitle: "Welcome to Coin Exchange",
+    })
 }])
-.run(["$rootScope", "$state", 'Auth', function($rootScope, $state, Auth) {
+.run(["$rootScope", "$state", 'Auth', 'DatabaseRef', function($rootScope, $state, Auth, DatabaseRef) {
   $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
     // We can catch the error thrown when the $requireSignIn promise is rejected
     // and redirect the user back to the home page
@@ -134,6 +171,8 @@ angular.module('coin', [
 .controller('loginController', LoginController)
 .controller('registerController', RegisterController)
 .controller('buyController', BuyController)
+.controller('usersController', UsersController)
+.controller('settingsController', SettingsController)
 
 .directive('a', preventClickDirective)
 .factory("Auth", ['$firebaseAuth', function($firebaseAuth) {
@@ -143,10 +182,19 @@ angular.module('coin', [
   return firebase.database().ref();
 })
 
-.factory('currency', ['$http', function ($http) {
-    var CURRENCY_ENDPOINT = 'http://apilayer.net/api/live?access_key=ae1c9e6e5ee4f5b211256b8a8c1b7713';
+.factory('api_calls', ['$http', 'DatabaseRef', function ($http, DatabaseRef) {
 
   return {
+    getExchangeRates: function (currency) {
+      // DatabaseRef
+    },
+
+    getConfiguration: function () {
+      var item = DatabaseRef.child('application_configuration');
+      item.once('value').then(function (snapshot) {
+        return snapshot.val();
+      })
+    }
 
   };
 }])
@@ -161,15 +209,27 @@ function preventClickDirective() {
   return directive;
 }
 
-NavbarController.inject = ['$rootScope', '$scope', '$state', 'Auth', 'DatabaseRef'];
-function NavbarController($rootScope, $scope, $state, Auth, DatabaseRef) {
+NavbarController.inject = ['$rootScope', '$scope', '$state', 'Auth', 'DatabaseRef', '$firebaseObject'];
+function NavbarController($rootScope, $scope, $state, Auth, DatabaseRef, $firebaseObject) {
 
     $scope.openModal = false;
 
      Auth.$onAuthStateChanged(function($firebaseUser) {
       if ($firebaseUser != null) {
-        $scope.id = $firebaseUser.uid;
+      var ref = firebase.database().ref('users').child(Auth.$getAuth().uid);
+      $scope.exchange_rates = $firebaseObject(ref);
+      $scope.id = $firebaseUser.uid;
         $scope.user = $firebaseUser.displayName;
+        var item = DatabaseRef.child('users').child($firebaseUser.uid);
+        item.on('value', function (snapshot) {
+          $scope.role = snapshot.val().type;
+          if($scope.role === 'admin'){
+            $scope.toggle_role = true;
+          }
+          else{
+            $scope.toggle_role = false;
+          }
+        })
         $scope.loggedIn = true;
       } else {
         $scope.loggedIn = false;
@@ -187,14 +247,15 @@ function NavbarController($rootScope, $scope, $state, Auth, DatabaseRef) {
 
 }
 
-LoginController.inject = ['$scope', '$state', '$firebaseAuth', 'Auth'];
-function LoginController($scope, $state, $firebaseAuth, Auth) {
+LoginController.inject = ['$scope', '$state', '$firebaseAuth', 'Auth', 'DatabaseRef'];
+function LoginController($scope, $state, $firebaseAuth, Auth, DatabaseRef) {
   $scope.login = function () {
     var auth = $firebaseAuth();
+    if((angular.isDefined($scope.email) && $scope.email != "") && (angular.isDefined($scope.password) && $scope.password != "")){
       auth.$signInWithEmailAndPassword($scope.email, $scope.password).then(function (response) {
         if(response.emailVerified){
             Materialize.toast("Welcome "+ response.displayName , 3000);
-              $state.go('app.index');
+                  $state.go('app.index');
         }
         else{
           Materialize.toast("Login failed. Please verify your account.", 3000);
@@ -204,12 +265,16 @@ function LoginController($scope, $state, $firebaseAuth, Auth) {
       }).catch(function (error) {
         Materialize.toast(error.message, 3000);
       })
+    }
+    else{
+      Materialize.toast("Please provide your login credentials");
+    }
 
   }
 }
 
-RegisterController.inject = ['$scope', '$firebaseAuth', '$state', 'DatabaseRef'];
-function RegisterController($scope, $firebaseAuth, $state, DatabaseRef) {
+RegisterController.inject = ['$scope', '$firebaseAuth', '$state', 'DatabaseRef', 'Auth'];
+function RegisterController($scope, $firebaseAuth, $state, DatabaseRef, Auth) {
 
   $scope.create_account = function () {
     var auth = $firebaseAuth();
@@ -237,14 +302,16 @@ function RegisterController($scope, $firebaseAuth, $state, DatabaseRef) {
                       dob: $scope.dob,
                       bankName: $scope.bank,
                       accountName: $scope.accountName,
-                      accountNumber: $scope.accountNumber
+                      accountNumber: $scope.accountNumber,
+                      type: 'client'
                     });
             $firebaseUser.sendEmailVerification();
             Materialize.toast("Your account has been created! Please check your mail to verify your account", 3000);
+            Auth.$signOut();
             $state.go('app.index');
-          }, function (error) {
-            Materialize.toast(error, 3000);
-          })
+          }).catch(function (error) {
+              Materialize.toast(error.message, 3000);
+            })
         })
     }
   }
@@ -254,15 +321,6 @@ BuyController.inject = ['$scope', 'DatabaseRef', '$firebaseObject', 'Auth'];
 function BuyController($scope, DatabaseRef, $firebaseObject, Auth) {
   var uid = Auth.$getAuth().uid;
 
-  $scope.exchange = false;
-
-  if($scope.exchange != true){
-    $scope.toggle_application_btn = "Apply for Currency Exchange";
-  }
-  else if($scope.exchange == true) {
-    $scope.toggle_application_btn = "Cancel Application";
-  }
-
   $scope.getMyApplications = function () {
       var item = DatabaseRef.child('application').child(uid);
       item.once('value').then(function (snapshot) {
@@ -271,6 +329,9 @@ function BuyController($scope, DatabaseRef, $firebaseObject, Auth) {
   }
 
   $scope.loadAccount = function () {
+    var ref = firebase.database().ref('exchange_rates');
+    $scope.exchange_rates = $firebaseObject(ref);
+
     var item = DatabaseRef.child('users').child(uid);
     item.once('value').then(function (snapshot) {
       $scope.form.bankName = snapshot.val().bankName;
@@ -279,55 +340,29 @@ function BuyController($scope, DatabaseRef, $firebaseObject, Auth) {
     }) 
   }
   
-  $scope.money = {
-        naira: "NAIRA", 
-        btc: "BTC", 
-        payza: "PAYZA", 
-        perfect_money: "PERFECT MONEY", 
-        web_money: "WEB MONEY", 
-        paypal: "PAYPAL"
-  };
+  $scope.money = [
+      {"currency": "NAIRA","unit": "10"}, 
+      {"currency": "BITCOIN","unit": "20"}, 
+      {"currency": "PAYZA","unit": "30"}, 
+      {"currency": "PERFECT MONEY","unit": "40"}, 
+      {"currency": "WEB MONEY","unit": "50"}, 
+      {"currency": "PAYPAL","unit": "60"}, 
+    ];
 
-
-    $scope.currency = function (value) {
-      switch(value){
-        case "NAIRA":
-          return 10;
-          break;
-        case "BTC":
-          return 20;
-          break;
-        case "PAYZA":
-          return 30;
-          break;
-        case "PERFECT MONEY":
-          return 40;
-          break;
-        case "WEB MONEY":
-          return 50;
-          break;
-        case "PAYPAL":
-          return 60;
-          break;
-        default:
-          return 0;
-      }
+  $scope.getTotalAmount = function () {
+    if((angular.isDefined($scope.form.buying) && $scope.form.buying != "") && (angular.isDefined($scope.form.worth) && $scope.form.worth != "")){
+      return parseInt($scope.form.worth * $scope.form.buying.unit);
     }
-    $scope.getTotalAmount = function () {
-      return $scope.form.units * $scope.currency($scope.form.buying);
+    else{
+      return "0.00";
     }
+  }
   $scope.apply = function () {
-    var timestamp = new Date().getTime();
-
-    // if(($scope.application.selling == undefined) || ($scope.application.buying == undefined) || ($scope.application.units == undefined)){
-    //   Materialize.toast("All fields are required.", 3000);
-    // }
-    // else{
       var data = {
           selling: $scope.form.selling,
-          buying: $scope.form.buying,
-          units: $scope.form.units,
-          amount: $scope.getTotalAmount() + ' ' + $scope.form.buying,
+          buying: $scope.form.buying.currency,
+          units: $scope.form.worth,
+          amount: $scope.getTotalAmount(),
           accountNumber: $scope.form.accountNumber,
           accountName: $scope.form.accountName,
           bankName: $scope.form.bankName, 
@@ -337,14 +372,82 @@ function BuyController($scope, DatabaseRef, $firebaseObject, Auth) {
       submit['users/'+ uid +'/applications/' + newKey] = data;
       DatabaseRef.update(submit).then(function () {
         Materialize.toast("Your application has been submitted. We'll get back to you in the next 24 hours", 3000);
+        $scope.form.selling = '';
+        $scope.form.buying = '';
+        $scope.form.worth = '';
       })
       .catch(function (error) {
         Materialize.toast(error, 3000);
       })
-    // }
   }
 }
 
-function currency_type(currency) {
-  
+SettingsController.inject = ['$scope', 'DatabaseRef', 'Auth', '$firebaseObject'];
+function SettingsController($scope, DatabaseRef, Auth, $firebaseObject) {
+
+  $scope.loadConfiguration = function () {
+    var ref = firebase.database().ref('application_configuration');
+    var config = $firebaseObject(ref);
+    // console.log("Loading");
+    // config.$loaded().then(function () {
+      // console.log("Loaded");
+      $scope.config = config;
+    // })
+  }
+
+  $scope.loadExchangeRates = function () {
+    var ref = firebase.database().ref('exchange_rates').child('currencies');
+    $scope.exchange_rates = $firebaseObject(ref);
+  }
+
+  $scope.updateConfiguration = function () {
+    DatabaseRef
+          .child('application_configuration')
+          .set({
+            application_name: $scope.config.application_name,
+            facebook_link: $scope.config.facebook_link,
+            twitter_link: $scope.config.twitter_link,
+            google_link: $scope.config.google_link
+          }).then(function () {
+            Materialize.toast("Your application details has been updated!", 3000);
+          }).catch(function (error) {
+            Materialize.toast(error.message, 3000)
+          })
+  };
+
+  $scope.updateExchangeRates = function () {
+    // Store data in json format (ie. key = { key => value})
+    $scope.money = {
+      "currencies": 
+      [
+      {"currency": "NAIRA","unit": $scope.exchange_rates[0].unit}, 
+      {"currency": "BITCOIN","unit": $scope.exchange_rates[1].unit}, 
+      {"currency": "PAYZA","unit": $scope.exchange_rates[2].unit}, 
+      {"currency": "PERFECT MONEY","unit": $scope.exchange_rates[3].unit}, 
+      {"currency": "WEB MONEY","unit": $scope.exchange_rates[4].unit}, 
+      {"currency": "PAYPAL","unit": $scope.exchange_rates[5].unit}, 
+    ]};
+    DatabaseRef
+              .child('exchange_rates')
+              .set($scope.money).then(function () {
+                Materialize.toast("Exchange Rates has been updated!", 3000);
+              }).catch(function (error) {
+                Materialize.toast(error.message, 3000);
+              })
+  }
+}
+
+UsersController.inject = ['$scope', 'DatabaseRef', '$firebaseObject', 'Auth'];
+function UsersController($scope, DatabaseRef, $firebaseObject, Auth) {
+  var uid = Auth.$getAuth().uid;
+
+  $scope.getMyApplications = function () {
+      var item = DatabaseRef.child('users');
+      item.once('value').then(function (snapshot) {
+        $scope.users = snapshot.val();
+        console.log($scope.users);
+      })
+  }
+
+
 }
